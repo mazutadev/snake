efb4d2bfc0f65db4e996290a2c613aa2a72eb73e
mazuta <mazuta.dev@gmail.com>
2023-06-06 12:16:41 +0300

    add generate game area

---
+++ main.py
@@ -0,0 +1,19 @@
+# def function game area
+
+def generate_game_area(width: int, length: int) -> list:
+    return [[" " for _ in range(width)] for _ in range(length)]
+
+
+def visualise_game_area(area: list) -> None:
+    for row in area:
+        for column in row:
+            print(column, end="")
+        print()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
